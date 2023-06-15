cbce69081c0b753216d8e6e6c2b6ff6bdc2568a0
Поремский <mr.poremskiy@mail.ru>
2023-06-15 11:32:13 +0300

    sorry

---
+++ SerializationLibrary/SerializationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace SerializationLibrary
+{
+    public class SerializationHelper
+    {
+        public static void Serialize<T>(T data, string filePath)
+        {
+            string serializedData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(filePath, serializedData);
+        }
+
+        public static T Deserialize<T>(string filePath)
+        {
+            string jsonData = File.ReadAllText(filePath);
+            T deserializedData = JsonConvert.DeserializeObject<T>(jsonData);
+            return deserializedData;
+        }
+    }
+}
--- bibl/MainWindow.xaml.cs
+++ bibl/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows;
+using SerializationLibrary;
+
 
 namespace bibl
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string data = dataTextBox.Text;
+            string filePath = "C:\\Users\\mrpor\\Desktop\\data.json";
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            SerializationHelper.Serialize(data, filePath);
+            MessageBox.Show("Данные сериализованы успешно в формате JSON!");
+
+            string deserializedData = SerializationHelper.Deserialize<string>(filePath);
+            MessageBox.Show("Данные десериализованы из формата JSON: " + deserializedData);
         }
     }
 }
